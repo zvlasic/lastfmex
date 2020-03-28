6c8dd39ac666ab7b8abe5333853bf5748a172063
zvlasic <zlayo1@gmail.com>
2020-03-28 22:16:59 +0100

    Initial

---
+++ lib/lastfmex.ex
@@ -0,0 +1,18 @@
+defmodule Lastfmex do
+  @moduledoc """
+  Documentation for `Lastfmex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lastfmex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lastfmex_test.exs
@@ -0,0 +1,8 @@
+defmodule LastfmexTest do
+  use ExUnit.Case
+  doctest Lastfmex
+
+  test "greets the world" do
+    assert Lastfmex.hello() == :world
+  end
+end
