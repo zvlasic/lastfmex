2a028d3d9c320cbeee1f256a5d78e13f1c941d26
zvlasic <zlayo1@gmail.com>
2021-01-12 21:14:26 +0100

    Use better specs

--- lib/scope/user.ex
+++ lib/scope/user.ex
@@ -1,6 +1,8 @@
 defmodule Lastfmex.User do
   import Lastfmex.Service
 
+  @type paginate :: [limit: integer(), page: integer()]
+
   @spec get_info(String.t()) :: map() | {:error, :not_found}
   def get_info(user_name), do: call("user.getinfo", user_name)
 
@@ -10,6 +12,8 @@ defmodule Lastfmex.User do
   @spec get_loved_tracks(String.t(), Keyword.t()) :: map() | {:error, :not_found}
   def get_loved_tracks(user_name, opts \\ []), do: call("user.getlovedtracks", user_name, opts)
 
-  @spec get_top_albums(String.t(), Keyword.t()) :: map() | {:error, :not_found}
-  def get_top_albums(user_name, opts \\ []), do: call("user.gettopalbums", user_name, opts)
+  @spec get_top_albums(String.t(), [paginate()]) :: map() | {:error, :not_found}
+  def get_top_albums(user_name, opts) do
+    call("user.gettopalbums", user_name, opts)
+  end
 end
