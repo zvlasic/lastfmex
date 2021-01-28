8fd2342d227f0ef2398791dcfb08f518b2dc575c
zvlasic <zlayo1@gmail.com>
2021-01-28 16:53:47 +0100

    Add period to user.get_top_albums in atom form :(

--- lib/scope/user.ex
+++ lib/scope/user.ex
@@ -2,6 +2,8 @@ defmodule Lastfmex.User do
   import Defmodulep
   requirep Lastfmex.Service, as: Service
 
+  @type period_length :: :overall | :"7day" | :"1month" | :"3month" | :"6month" | :"12month"
+
   @spec get_info(user_name :: String.t()) :: map() | {:error, :not_found}
   def get_info(user_name), do: Service.call("user.getinfo", user_name)
 
@@ -18,7 +20,7 @@ defmodule Lastfmex.User do
   def get_loved_tracks(user_name, opts \\ []),
     do: Service.call("user.getlovedtracks", user_name, opts)
 
-  @spec get_top_albums(String.t(), limit: integer(), page: integer()) ::
+  @spec get_top_albums(String.t(), limit: integer(), page: integer(), period: period_length()) ::
           map() | {:error, :not_found}
   def get_top_albums(user_name, opts \\ []),
     do: Service.call("user.gettopalbums", user_name, opts)
