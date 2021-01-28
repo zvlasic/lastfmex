78437739cff8d3065b7813132573f48e066b46e3
zvlasic <zlayo1@gmail.com>
2021-01-28 10:21:25 +0100

    Bump elixir and remove unneeded deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lastfmex.MixProject do
     [
       app: :lastfmex,
       version: "0.1.0",
-      elixir: "~> 1.10.2",
+      elixir: "~> 1.11.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,7 @@ defmodule Lastfmex.MixProject do
     [
       {:credo, "~> 1.3"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:httpoison, "~> 1.6"},
-      {:poison, "~> 4.0"}
+      {:httpoison, "~> 1.6"}
     ]
   end
 end
