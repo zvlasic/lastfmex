bea2ff6e997703280f1b7bfa0e158cda4cb35c8f
zvlasic <zlayo1@gmail.com>
2021-02-02 15:28:54 +0100

    Shorten fn names

--- lib/scope/chart.ex
+++ lib/scope/chart.ex
@@ -2,13 +2,13 @@ defmodule Lastfmex.Chart do
   import Defmodulep
   requirep Lastfmex.Service, as: Service
 
-  @spec get_top_artists(String.t()) :: {:ok, map()} | {:error, :not_found}
-  def get_top_artists(user_name), do: Service.call("chart.gettopartists", user_name)
+  @spec top_artists(String.t()) :: {:ok, map()} | {:error, :not_found}
+  def top_artists(user_name), do: Service.call("chart.gettopartists", user_name)
 
-  @spec get_top_tags(String.t(), Keyword.t()) :: {:ok, map()} | {:error, :not_found}
-  def get_top_tags(user_name, opts \\ []), do: Service.call("chart.gettoptags", user_name, opts)
+  @spec top_tags(String.t(), Keyword.t()) :: {:ok, map()} | {:error, :not_found}
+  def top_tags(user_name, opts \\ []), do: Service.call("chart.gettoptags", user_name, opts)
 
-  @spec get_top_tracks(String.t(), Keyword.t()) :: {:ok, map()} | {:error, :not_found}
-  def get_top_tracks(user_name, opts \\ []),
+  @spec top_tracks(String.t(), Keyword.t()) :: {:ok, map()} | {:error, :not_found}
+  def top_tracks(user_name, opts \\ []),
     do: Service.call("chart.gettoptracks", user_name, opts)
 end
--- test/lastfmex_test.exs
+++ test/lastfmex_test.exs
@@ -4,7 +4,7 @@ defmodule LastfmexTest do
 
   test "get request" do
     use_cassette "httpoison_get" do
-      {:ok, response} = Lastfmex.User.get_top_albums("evil_zlayo", limit: 3)
+      {:ok, response} = Lastfmex.User.top_albums("evil_zlayo", limit: 3)
 
       assert Enum.any?(Map.keys(response), &(&1 == :topalbums))
       assert response.topalbums."@attr".user == "evil_zlayo"
@@ -14,7 +14,7 @@ defmodule LastfmexTest do
 
   test "report error" do
     use_cassette "httpoison_get_error" do
-      {:error, error} = Lastfmex.User.get_top_albums("evil_zlayo", limit: 10_000)
+      {:error, error} = Lastfmex.User.top_albums("evil_zlayo", limit: 10_000)
 
       assert error.error == 6
       assert error.message == "limit param out of bounds (1-1000)"
